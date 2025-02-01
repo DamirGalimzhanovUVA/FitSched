434db23394672597711adf0955414a5dda04f8cd
Дамир Галимжанов <damir.galimzhanov@freedompay.kz>
2025-02-01 18:25:20 -0500

    the previuos commit

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,3 @@
 <template>
-  <div>
-    <Login v-if="!isLoggedIn" @login-success="isLoggedIn = true" />
-    <div v-if="isLoggedIn">
-      <h2>Welcome to the Dashboard</h2>
-      <button @click="isLoggedIn = false">Logout</button>
-    </div>
-  </div>
+  <router-view></router-view>
 </template>
-
-<script>
-import Login from './components/Login.vue';
-
-export default {
-  components: { Login },
-  data() {
-    return {
-      isLoggedIn: false
-    };
-  }
-};
-</script>
---
+++ src/components/Dashboard.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+      <h2>Welcome to the Dashboard</h2>
+      <button @click="logout">Logout</button>
+    </div>
+  </template>
+  
+  <script>
+  import { useRouter } from 'vue-router';
+  
+  export default {
+    setup() {
+      const router = useRouter();
+  
+      const logout = () => {
+        router.push('/');
+      };
+  
+      return { logout };
+    }
+  };
+  </script>
